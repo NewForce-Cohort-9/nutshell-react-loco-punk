9a0b2ccbfb877a3e871031be0101573d0c27fedc
Abby Trombley <abbyt2024@gmail.com>
2024-06-05 11:08:40 -0400

    nothing

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -17,3 +17,37 @@
 //When the user is viewing their messages
 //Then there should be an affordance that allows the user to modify the message
 
+
+//Should be on app.jsx
+import { useState, useEffect } from "react"
+import { getAllMessages } from "./services/messageService"
+//import css
+
+export const Chat = () => {
+    const [allMessages, setAllMessages] = useState([])
+    const [makeEdit, setMakeEdit] = useState([])
+
+    useEffect(() => {
+        getAllMessages().then((messagesArray) => {
+            console.log("chats")
+        })
+    }, [])
+
+    return (
+        <div className="chat-container">
+            <h2>Messages</h2>
+            <div>
+                <button className="edit-btn">Edit</button>
+            </div>
+            <article className="chats">
+                {allMessages.map(message => {
+                    return (
+                        <section className="chats">
+                            <header className="chat-inside"> #{messages.message}</header>
+                        </section>
+                    )
+                })}
+            </article>
+        </div>
+    )
+}
--- src/Services/messageService.jsx
+++ src/Services/messageService.jsx
@@ -2,36 +2,4 @@ export const getAllMessages = () => {
     return fetch(`http://localhost:8088/messages`).then((res) => res.json())
 }
 
-//Should be on app.jsx
-import { useState, useEffect } from "react"
-import { getAllChats } from "./services/messageService"
-//import css
 
-export const App = () => {
-    const [allMessages, setAllMessages] = useState([])
-    const [makeEdit, setMakeEdit] = useState([])
-
-    useEffect(() => {
-        getAllMessages().then((messagesArray) => {
-            console.log("chats")
-        })
-    }, [])
-
-    return (
-        <div className="chat-container">
-            <h2>Messages</h2>
-            <div>
-                <button className="edit-btn">Edit</button>
-            </div>
-            <article className="chats">
-                {allMessages.map(message => {
-                    return (
-                        <section className="chats">
-                            <header className="chat-inside"> #{messages.message}</header>
-                        </section>
-                    )
-                })}
-            </article>
-        </div>
-    )
-}
