567d3f6c8db0efb88bf7b0a3273e1b0e5a30e9fe
Will Moran <will.c.moran@gmail.com>
2024-06-05 11:05:35 -0400

    fixed

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { Outlet, Route, Routes } from "react-router-dom"
 import { Dashboard } from "../components/dashboard/Dashboard.jsx"
 import { AllArticles } from "../components/articles/AllArticles.jsx"
+import { NavBar } from "../components/Chat/Nav/NavBar.jsx"
 
 export const ApplicationViews = () => {
     const [currentUser, setCurrentUser] = useState({})
@@ -16,6 +17,7 @@ export const ApplicationViews = () => {
                 path="/"
                 element={
                     <>
+                        <NavBar />
                         <Outlet />
                     </>
                 }
