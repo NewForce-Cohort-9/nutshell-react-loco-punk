bc694941b565d3d691cbe5d3f631e6dbcac77dac
Abby Trombley <abbyt2024@gmail.com>
2024-06-07 11:34:38 -0400

    nothing

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -3,13 +3,12 @@ import { Outlet, Route, Routes } from "react-router-dom"
 import { NavBar } from "../Components/Chat/Nav/NavBar.jsx"
 import { Dashboard } from "../Components/dashboard/Dashboard.jsx"
 import { AllArticles } from "../Components/articles/AllArticles.jsx"
-import { NavBar } from "../Components/Chat/Nav/NavBar.jsx"
-import { Chat } from "../Components/Chat/Chat.jsx"
-import { editMessage } from "../Components/Chat/editMessage.jsx"
 import { NewArticle } from "../Components/articles/NewArticle.jsx"
 import { Login } from "../Components/auth/Login.jsx"
 import { Register } from "../Components/auth/Register.jsx"
 import { ArticleEdit } from "../Components/articles/ArticleEdit.jsx"
+
+
 export const ApplicationViews = () => {
     const [currentUser, setCurrentUser] = useState(0)
 
@@ -25,9 +24,7 @@ export const ApplicationViews = () => {
                 element={
                     <>
                         <NavBar />
-                        <NavBar />
                         <Outlet />
-
                     </>
                 }
             >
@@ -49,13 +46,7 @@ export const ApplicationViews = () => {
                 </Route>
                 
 
-                <Route path="articles" element={<AllArticles />} />
-                <Route path="messages" element={<Chat />} /> 
-                    <Route path=":messageId/edit" element={<editMessage />}>
-                    
-                </Route>
             </Route>
         </Routes>
     )
 }
- 
